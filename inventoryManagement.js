9647c1c63e3111dc7afd8ed6463a1547e649fa19
amercado1199 <amercado1199@gmail.com>
2026-03-06 00:02:47 -0500

    Completed arrays lab

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,12 +1,25 @@
-// Write your code here
+let products = ["Laptop", "Phone", "Headphones", "Monitor"];
 
+function logFirstProduct() {
+    console.log(products[0]);
+}
 
+function addProduct(productName) {
+    products.push(productName);
+}
 
-// Export the necessary parts for testing
+function updateProductName(index, newName) {
+    products[index] = newName;
+}
+
+function removeLastProduct() {
+    products.pop();
+}
+
 module.exports = {
-  logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
-  addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
-  updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
-  removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
-  products
+    products,
+    logFirstProduct,
+    addProduct,
+    updateProductName,
+    removeLastProduct
 };
